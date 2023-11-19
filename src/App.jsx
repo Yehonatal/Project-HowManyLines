679a8db1c0ef4171c20dcc00a48cb32d5575040a
yonatan afewerk <yonatanafewerk@gmail.com>
2023-11-20 01:31:59 +0300

    Development: header, search added 🤌🏾

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,6 @@ function App() {
                 <Search />
             </main>
             <div className="absolute flex gap-10 opacity-[0.25] flex-wrap items-center justify-center bg-red-400 z-0  top-0 left-0 right-0 bottom-0 select-none"></div>
-            <div className="absolute flex gap-10 flex-wrap items-center justify-center z-50  top-0 left-0 right-0 bottom-0 select-none">
-                <div className="scale-[5]">🏗️</div>
-                <h1 className="text-orange-300 z-20 font-extrabold text-4xl">
-                    UNDER DEVELOPMENT . . .
-                </h1>
-            </div>
         </>
     );
 }
